07391442e46d5447c46c19dc32e7c2979f0d90bf
Katherine Frields <119017028+katherinefrields@users.noreply.github.com>
2025-10-17 16:11:50 -0700

    moved batch_scripts to climsim_scripts folder

---
+++ climsim_scripts/batch_scripts/v4.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#SBATCH -A m4334
+#SBATCH -C cpu
+#SBATCH -q debug
+#SBATCH -t 00:10:00
+#SBATCH --ntasks-per-node 32
+#SBATCH --cpus-per-task 1
+#SBATCH --mem=128G 
+#SBATCH --mail-user=frieldskatherine@gmail.com
+#SBATCH --mail-type=ALL
+#SBATCH --output=out_%j.out
+#SBATCH --error=eo_%j.err
+
+free -h
+ulimit -a
+
+
+podman-hpc run  --rm -it --volume="/pscratch/sd/k/kfrields/climsim-online-data/inputdata:/storage/inputdata" \
+    --volume "/pscratch/sd/k/kfrields/climsim-online-data/shared_e3sm:/storage/shared_e3sm" \
+    --volume "/pscratch/sd/k/kfrields/climsim-online-data/scratch:/scratch" \
+    --volume "/dev/shm:/dev/shm" \
+    climsim:podman python E3SM/climsim_scripts/example_job_submit_nnwrapper_v4_constrained.py
---
+++ climsim_scripts/batch_scripts/v4_run.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#SBATCH -A m4334
+#SBATCH -C cpu
+#SBATCH -q regular
+#SBATCH -t 02:00:00
+#SBATCH --ntasks-per-node 32
+#SBATCH --cpus-per-task 1
+#SBATCH --mem=128G 
+#SBATCH --mail-user=frieldskatherine@gmail.com
+#SBATCH --mail-type=ALL
+#SBATCH --output=out_%j.out
+#SBATCH --error=eo_%j.err
+
+free -h
+ulimit -a
+
+#I moved this file so you might have to cd to the top in order to run it correctly
+
+podman-hpc run  --rm -it --volume="/pscratch/sd/k/kfrields/climsim-online-data/inputdata:/storage/inputdata" \
+    --volume "/pscratch/sd/k/kfrields/climsim-online-data/shared_e3sm:/storage/shared_e3sm" \
+    --volume "/pscratch/sd/k/kfrields/climsim-online-data/scratch:/scratch" \
+    --volume "/pscratch/sd/k/kfrields/hugging/E3SM-MMF_saved_models:/hugging" \
+    --volume "/dev/shm:/dev/shm" \
+    climsim:podman python E3SM/climsim_scripts/example_job_submit_nnwrapper_v4_constrained.py
